21b79a91d6df506aaf26a8c084f19cebc81049f1
Dmitri Lapin <lapin@cvisionlab.com>
2015-12-17 11:31:25 +0300

    add c++11 initializer list support for choices if supported by compiler
    add corresponding test in test.cpp

--- test.cpp
+++ test.cpp
@@ -101,6 +101,9 @@ int main(int argc, char *argv[])
   parser.add_option("-c", "--complex") .action("store") .type("complex");
   char const* const choices[] = { "foo", "bar", "baz" };
   parser.add_option("-C", "--choices") .choices(&choices[0], &choices[3]);
+#if __cplusplus >= 201103L
+  parser.add_option("--choices-list") .choices({"item1", "item2", "item3"});
+#endif
   parser.add_option("-m", "--more") .action("append");
   parser.add_option("--more-milk") .action("append_const") .set_const("milk");
   parser.add_option("--hidden") .help(SUPPRESS_HELP);
@@ -133,6 +136,7 @@ int main(int argc, char *argv[])
   }
   cout << "complex: " << c << endl;
   cout << "choices: " << (const char*) options.get("choices") << endl;
+  cout << "choices-list: " << (const char*) options.get("choices-list") << endl;
   cout << "more: ";
   for_each(options.all("more").begin(), options.all("more").end(), Output(", "));
   cout << "more_milk: ";
